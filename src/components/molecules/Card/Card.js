6449fd5b26f8ba179950825a688862e9c2a57ed4
Kamil Hajduk <khajduk5@gmail.com>
2020-02-26 22:50:34 +0100

    First molecule added

---
+++ src/components/molecules/Card/Card.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
+import Heading from 'components/atoms/Heading/Heading';
+import Button from 'components/atoms/Button/Button';
+
+const StyledWrapper = styled.div`
+    min-height: 380px;
+    box-shadow: 0 10px 30px -10px hsla(0, 0%, 0%, 0.1);
+    border-radius: 10px;
+    overflow: hidden;
+    position: relative;
+    display: grid;
+    grid-template-rows: 0.25fr 1fr;
+`;
+
+const InnerWrapper = styled.div`
+    padding: 17px 30px;
+    background-color: ${({ yellow, theme }) => (yellow ? theme.primary : 'white')};
+
+    ${({ flex }) => flex && css`
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    `}
+`;
+
+const DateInfo = styled(Paragraph)`
+    font-weight: ${({ theme }) => theme.bold};
+    font-size: ${({ theme }) => theme.fontSize.xs};
+`;
+
+const StyleHeading = styled(Heading)`
+    margin: 5px 0 0;
+`;
+
+const Card = () => (
+  <StyledWrapper>
+    <InnerWrapper yellow>
+      <StyleHeading>Hello world</StyleHeading>
+      <DateInfo>3 days</DateInfo>
+    </InnerWrapper>
+    <InnerWrapper flex>
+      <Paragraph>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </Paragraph>
+      <Button secondary>REMOVE</Button>
+    </InnerWrapper>
+  </StyledWrapper>
+);
+
+export default Card;
